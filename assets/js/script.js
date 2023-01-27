cde2d74b4db5b87afa3f10d441e1171446fb6169
siobhain <sobrien@live.ie>
2023-01-27 19:32:12 +0000

    setup structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
 console.log("Connected");
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+ function displaySubtractQuestion() {
+
+ }
+
+ function displayMultiplyQuestion() {
+
+ }
+ 
